3105cbd3a5d68295f67416d6c94158beb80f2347
PatKowal <148785927+PatKowal@users.noreply.github.com>
2025-06-08 17:38:11 +0200

    Update login.php

--- PHP/login.php
+++ PHP/login.php
@@ -1,6 +1,40 @@
 <?php
-session_start();
-require_once '../DbConnect.php';
 
-// tutaj dodać skrypt logowania :)
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	$email = trim($_POST['email']);
+    $haslo = $_POST['password'];
+
+	if(empty($email) || empty($haslo)) {
+		echo "Brakuje danych logowania";
+		exit;
+	}
+	mysqli_report(MYSQLI_REPORT_OFF);
+	@$conn = mysqli_connect("localhost", "root", "", "firmakurierska");
+	if (!$conn) {
+            http_response_code(500);
+            echo "Bład połączenia z bazą";
+            exit;
+	}
+	$email = mysqli_real_escape_string($conn, $email);
+	$sql = "SELECT haslo_hash,login FROM użytkownicy WHERE email='$email'";
+    $result = mysqli_query($conn, $sql);
+
+	if(!$result) {
+		echo "Błąd zapytania";
+		exit;
+	}
+	if(mysqli_num_rows($result) == 0){
+		echo "Nieprawidłowy email lub hasło";
+		exit;
+	}
+	$row = mysqli_fetch_assoc($result);
+	if (password_verify($haslo, $row['haslo_hash'])){
+		session_start();
+		$_SESSION['login'] = $row['login'];
+		echo "OK";
+	} else {
+		echo "Nieprawidłowy email lub hasło";
+	}
+	mysqli_close($conn);
+}
 
